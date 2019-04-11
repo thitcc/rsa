9a003d40a44f88ab9a7d16195108941c94ef35c2
Thiago <thitcc@hotmail.com>
2019-04-11 00:27:46 -0300

    Changing while for do while, with menu print

--- src/ic/ufal/br/Main.java
+++ src/ic/ufal/br/Main.java
@@ -8,17 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("RSA Encryption: \n" +
-                "1 - Generate Public Key\n" +
-                "2 - Encrypt\n" +
-                "3 - Decrypt\n" +
-                "0 - Exit");
-
         InvokerHub hub = new InvokerHub(4);
 
-        while (hub.onSelect(intInput())) {
-            System.out.println("teste");
-        }
+        do {
+            System.out.println("RSA Encryption: \n\n" +
+                    "1 - Generate Public Key\n" +
+                    "2 - Encrypt\n" +
+                    "3 - Decrypt\n" +
+                    "0 - Exit");
+        } while (hub.onSelect(intInput()));
 
     }
 }
