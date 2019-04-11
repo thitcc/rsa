5b10a6d8347ebc2af30e8a2f8eef9f99a98f7848
Thiago <thitcc@hotmail.com>
2019-04-11 00:34:45 -0300

    Adding index out of bounds treatment

--- src/ic/ufal/br/treatment/IntInputHandling.java
+++ src/ic/ufal/br/treatment/IntInputHandling.java
@@ -14,10 +14,13 @@ public class IntInputHandling {
             try {
                 System.out.print("\n>> ");
                 value = Integer.parseInt(input.next());
+                if (value < 0 || value >= 4) throw new ArrayIndexOutOfBoundsException();
                 correctInput = true;
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 System.out.print("You typed a value that is not a integer, please try again\n" +
                         e.getMessage());
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.print("This command: " + value + " does not exist, please try again");
             }
         }
 
