f3ba602885e267d8543ecd28dadd3fd913e7e41f
Thiago <thitcc@hotmail.com>
2019-04-10 23:55:53 -0300

    Integer input handling

---
+++ src/ic/ufal/br/treatment/IntInputHandling.java
@@ -0,0 +1,27 @@
+package ic.ufal.br.treatment;
+
+import java.util.Scanner;
+
+public class IntInputHandling {
+
+    public static int intInput() {
+        Scanner input = new Scanner(System.in);
+
+        boolean correctInput = false;
+        int value = 0;
+
+        while (!correctInput) {
+            try {
+                System.out.print("\n>> ");
+                value = Integer.parseInt(input.next());
+                correctInput = true;
+            } catch(NumberFormatException e) {
+                System.out.print("You typed a value that is not a integer, please try again\n" +
+                        e.getMessage());
+            }
+        }
+
+        return value;
+    }
+
+}
