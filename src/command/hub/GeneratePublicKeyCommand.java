881b59ea86b62ba98670dde4e376853ab618c30c
Thiago <thitcc@hotmail.com>
2019-04-11 17:40:31 -0300

    P and Q entries done w/ prime treatment

--- src/command/hub/GeneratePublicKeyCommand.java
+++ src/command/hub/GeneratePublicKeyCommand.java
@@ -2,9 +2,28 @@ package command.hub;
 
 import command.ICommand;
 
+import static treatment.IntInputHandling.intInput;
+import static treatment.PrimeHandling.isPrime;
+
 public class GeneratePublicKeyCommand implements ICommand {
     @Override
     public boolean execute() {
+        boolean correctInput = false;
+        int p, q;
+
+        do {
+            System.out.print("Please enter the first prime ");
+            p = intInput();
+            System.out.print("Please enter the second prime ");
+            q = intInput();
+            try {
+                if (!isPrime(p) || !isPrime(q)) throw new NumberFormatException();
+                correctInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("You entered a non-prime value, try again");
+            }
+        } while (!correctInput);
+
         return true;
     }
 }
