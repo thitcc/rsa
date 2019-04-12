3a6c8c7f388d2fdd1e41d1a3987a50d2f4cc31ec
Ricardo Alves <ras@ic.ufal.br>
2019-04-11 21:22:18 -0300

    Adding EncryptCommand

---
+++ src/command/hub/EncryptCommand.java
@@ -0,0 +1,45 @@
+package command.hub;
+
+import command.ICommand;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class EncryptCommand implements ICommand {
+    @Override
+    public boolean execute() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nMessage:");
+        String message = input.nextLine().toUpperCase();
+        String invalidChars = message.replaceAll("[ A-Z]", "");
+
+        if (!invalidChars.isEmpty()) {
+            System.out.println("Message contains invalid characters");
+            return false;
+        }
+
+        System.out.print("\nPublic Key (n):\n>> ");
+        BigInteger publicKey = input.nextBigInteger();
+
+        System.out.print("Exponent (e):\n>> ");
+        BigInteger exponent = input.nextBigInteger();
+
+        char[] codedMessage = message.replace(' ','[').toCharArray();
+        BigInteger[] encryptedMessage = new BigInteger[codedMessage.length];
+        message = "";
+
+        for (int i = 0; i < codedMessage.length; i++) {
+            BigInteger aux = BigInteger.valueOf(codedMessage[i] - 'A');
+            encryptedMessage[i] = aux.modPow(exponent, publicKey);
+
+            message += encryptedMessage[i] + ",";
+        }
+
+        System.out.println("Encrypted message: " + message.split(",$")[0]);
+
+        return true;
+    }
+
+}
