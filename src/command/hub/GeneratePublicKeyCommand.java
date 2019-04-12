7be96fc84d346ceeee54711962ad859efea29bda
Thiago <thitcc@hotmail.com>
2019-04-12 14:44:01 -0300

    Generate Public Key treatment done

--- src/command/hub/GeneratePublicKeyCommand.java
+++ src/command/hub/GeneratePublicKeyCommand.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 import java.math.BigInteger;
 
 import static treatment.BigIntInputHandling.bigIntegerInput;
+import static treatment.GeneratePublicKeyHandling.*;
 
 public class GeneratePublicKeyCommand implements ICommand {
     @Override
     public boolean execute() {
-        boolean correctInput = false;
+        boolean correctInput;
         BigInteger p, q;
 
         do {
@@ -21,52 +22,28 @@ public class GeneratePublicKeyCommand implements ICommand {
             System.out.print("Please enter the second prime or 0 to exit ");
             q = bigIntegerInput();
 
-
-            try {
-                if (p.compareTo(BigInteger.ZERO) == 0 || q.compareTo(BigInteger.ZERO) == 0) {
-                    System.out.println("\nReturning to menu\n");
-                    return true;
-                }
-                else if (!p.isProbablePrime(7) || !q.isProbablePrime(13)) throw new NumberFormatException();
-                correctInput = true;
-            } catch (NumberFormatException e) {
-                System.out.println("You entered a non-prime value, try again\n" +
-                        "Suggestion: " + p.nextProbablePrime() + " and " + q.nextProbablePrime());
+            if (p.compareTo(BigInteger.ZERO) == 0 || q.compareTo(BigInteger.ZERO) == 0) {
+                System.out.println("\nReturning to menu\n");
+                return true;
             }
+
+            correctInput = primesTreatment(p, q);
         } while (!correctInput);
 
         BigInteger n = p.multiply(q);
         BigInteger pn = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
         BigInteger e;
 
-        correctInput = false;
-
         do {
             System.out.print("Please enter a coprime with " + pn + " (Phi(n)) or 0 to exit");
             e = bigIntegerInput();
-            try {
-                if (e.compareTo(BigInteger.ZERO) < 0) {
-                    if (e.compareTo(BigInteger.ZERO) == 0) {
-                        System.out.println("\nReturning to menu\n");
-                        return true;
-                    } else throw new UnsupportedOperationException();
-                }
-                else if (e.compareTo(pn) > 0) throw new IllegalStateException();
-                else if (!e.isProbablePrime(7)) throw new NumberFormatException();
-                else if (pn.gcd(e).compareTo(BigInteger.ONE) != 0) throw new ArithmeticException();
-                correctInput = true;
-            } catch (UnsupportedOperationException f){
-                System.out.println("You entered a negative number, try again");
-            } catch (IllegalStateException f) {
-                System.out.println("You entered a prime number bigger than Phi(n), try again\n" +
-                        "Suggestion: " + n.subtract(pn).nextProbablePrime());
-            } catch (NumberFormatException f) {
-                System.out.println("You entered a non-prime value, try again\n" +
-                        "Suggestion: " + e.nextProbablePrime());
-            } catch (ArithmeticException f) {
-                System.out.println("You entered a non-coprime value, try gain\n" +
-                        "Suggestion: " + e.nextProbablePrime());
+
+            if (e.compareTo(BigInteger.ZERO) == 0) {
+                System.out.println("\nReturning to menu\n");
+                return true;
             }
+
+            correctInput = coprimeTreatment(e, pn, n);
         } while (!correctInput);
 
         try {
---
+++ src/treatment/GeneratePublicKeyHandling.java
@@ -0,0 +1,41 @@
+package treatment;
+
+import java.math.BigInteger;
+
+public class GeneratePublicKeyHandling {
+
+    public static boolean primesTreatment(BigInteger p, BigInteger q) {
+        try {
+            if (!p.isProbablePrime(1) || !q.isProbablePrime(1)) throw new NumberFormatException();
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("You entered a non-prime value, try again\n" +
+                    "Suggestion: " + p.nextProbablePrime() + " and " + q.nextProbablePrime());
+        }
+
+        return false;
+    }
+
+    public static boolean coprimeTreatment(BigInteger e, BigInteger pn, BigInteger n) {
+        try {
+            if (e.compareTo(BigInteger.ZERO) < 0) throw new UnsupportedOperationException();
+            else if (e.compareTo(pn) > 0) throw new IllegalStateException();
+            else if (!e.isProbablePrime(1)) throw new NumberFormatException();
+            else if (pn.gcd(e).compareTo(BigInteger.ONE) != 0) throw new ArithmeticException();
+            return true;
+        } catch (UnsupportedOperationException f){
+            System.out.println("You entered a negative number, try again");
+        } catch (IllegalStateException f) {
+            System.out.println("You entered a prime number bigger than Phi(n), try again\n" +
+                    "Suggestion: " + n.subtract(pn).nextProbablePrime());
+        } catch (NumberFormatException f) {
+            System.out.println("You entered a non-prime value, try again\n" +
+                    "Suggestion: " + e.nextProbablePrime());
+        } catch (ArithmeticException f) {
+            System.out.println("You entered a non-coprime value, try gain\n" +
+                    "Suggestion: " + e.nextProbablePrime());
+        }
+
+        return false;
+    }
+}
